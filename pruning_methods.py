2ef52156beaa502acdf20c9016b64a47db4d9185
Roope Niemi <roope.niemi@gmail.com>
2025-02-11 16:19:50 +0100

    PDP structured pruning for linear layers. New pruning method ActivationPruning. Skip activation replacement with quantized version if quantization is not enabled

--- pruning_methods.py
+++ pruning_methods.py
@@ -17,6 +17,8 @@ def get_pruning_layer(config, layer, out_size):
             return ContinuousSparsification(config, layer, out_size)
         elif config.pruning_method == "pdp":
             return PDP(config, layer, out_size)
+        elif config.pruning_method == "continual_learning":
+            return ActivationPruning(config, layer, out_size)
 
 def get_threshold_size(config, size, weight_shape):
     if config.threshold_type == "layerwise":
@@ -27,6 +29,68 @@ def get_threshold_size(config, size, weight_shape):
         return (weight_shape[0], np.prod(weight_shape[1:]))
 
 
+
+class ActivationPruning(nn.Module):
+
+    def __init__(self, config, layer, out_size, *args, **kwargs):
+            super(ActivationPruning, self).__init__(*args, **kwargs)
+            self.config = config
+            self.act_type = "relu"
+            self.t = 0
+            self.layer_type = "linear" if isinstance(layer, nn.Linear) else "conv"
+            self.shape = (1, layer.weight.shape[1]) if self.layer_type == "linear" else layer.weight.shape
+            self.mask = torch.ones(self.shape, requires_grad=False).to(layer.weight.device)
+            self.activations = None
+            self.register_forward_hook(self.collect_output)
+
+    def collect_output(self, module, input, output):
+        if not self.training:
+            return
+        if self.activations is None:
+            self.activations = torch.zeros(size=output.shape, dtype=output.dtype, device=self.mask.device)
+        self.t += 1
+        gt_zero = (output > 0).float()
+        self.activations += gt_zero
+        if self.t % self.config.t_delta == 0:
+            self.t = 0
+            pct_active = self.activations / self.config.t_delta
+            if self.layer_type == "linear":
+                pct_active_avg = torch.mean(pct_active, dim=0)
+                self.mask = (pct_active_avg > self.config.threshold).float().unsqueeze(0)
+            else:
+                pct_active = pct_active.view(pct_active.shape[0], pct_active.shape[1], -1)
+                pct_active_avg = torch.mean(pct_active, dim=-1)
+                pct_active_above_threshold = (pct_active_avg > self.config.threshold).float()
+                self.mask = (pct_active_above_threshold).unsqueeze(-1).unsqueeze(-1)
+            self.activations = None
+
+    def build(self, weight):
+        # Since this is a torch layer, do nothing
+        pass
+
+    def forward(self, weight):
+        return self.mask * weight
+    
+    def post_pre_train_function(self):
+        pass
+
+    def pre_epoch_function(self, epoch, total_epochs):
+        pass
+    
+    def post_round_function(self):
+        pass
+    
+    def pre_finetune_function(self):
+        pass
+
+    def calculate_additional_loss(self):
+        return 0
+    
+    def get_layer_sparsity(self, weight):
+        pass
+    def post_epoch_function(self, epoch, total_epochs):
+        pass
+
 class PDP(keras.layers.Layer):
     def __init__(self, config, layer, out_size, *args, **kwargs):
         super(PDP, self).__init__(*args, **kwargs)
@@ -36,6 +100,8 @@ class PDP(keras.layers.Layer):
         self.is_pretraining = True
         self.config = config
         self.fine_tuning = False
+        self.layer_type = "linear" if isinstance(layer, nn.Linear) else "conv"
+
     def build(self, input_shape):
         input_shape_concatenated = list(input_shape) + [1]
         self.softmax_shape = input_shape_concatenated
@@ -57,7 +123,10 @@ class PDP(keras.layers.Layer):
         if self.fine_tuning:
             return (self.mask >= 0.5).float()
         if self.config.structured_pruning:
-            mask = self.get_mask_structured(weight)
+            if self.layer_type == "conv":
+                mask = self.get_mask_structured_channel(weight)
+            else:
+                mask = self.get_mask_structured_linear(weight)
         else:
             mask = self.get_mask(weight)
         self.mask = (mask >= 0.5).float()
@@ -67,10 +136,33 @@ class PDP(keras.layers.Layer):
         self.fine_tuning = True
         self.mask = (self.mask  >= 0.5).float()
 
-
-    def get_mask_structured(self, weight):
+    def get_mask_structured_linear(self, weight):
         """
-        Structured pruning. Use the l2 norm of the channels instead of the absolute weight values to calculate threshold point t. ]
+        Structured pruning. Use the l2 norm of the neurons instead of the absolute weight values to calculate threshold point t. 
+        Prunes whole neurons.
+        """
+        if self.is_pretraining:
+            return self.mask
+        norm = ops.norm(weight, axis=0, ord=2, keepdims=True)
+        norm_flat = ops.reshape(norm, -1)
+        # Do top_k for all neuron norms. Returns sorted array, just use the values on both sides of the threshold (sparsity * size(norm)) to calculate t directly
+        W_all, _ = ops.top_k(norm_flat, ops.size(norm_flat))
+        lim = ops.clip(int((1-self.r) * ops.size(W_all)), 0, ops.size(W_all) - 2)
+
+        Wh = W_all[lim]
+        Wt = W_all[lim + 1]
+        #norm = ops.expand_dims(norm, -1)
+        t = ops.ones(norm.shape) * 0.5 * (Wh + Wt) 
+        soft_input = ops.concatenate((t ** 2, norm ** 2), axis=0) / self.temp
+        softmax_result = ops.softmax(soft_input, axis=0)
+        zw, mw = ops.unstack(softmax_result, axis=0)
+        mw = ops.expand_dims(mw, 0)
+        self.mask = mw
+        return mw
+
+    def get_mask_structured_channel(self, weight):
+        """
+        Structured pruning. Use the l2 norm of the channels instead of the absolute weight values to calculate threshold point t.
         Prunes whole channels.
         """
         if self.is_pretraining:
@@ -96,7 +188,6 @@ class PDP(keras.layers.Layer):
         self.mask = mw
         return mw
     
-
     def get_mask(self, weight):
         if self.is_pretraining:
             self.mask = ops.ones(weight.shape)
@@ -122,7 +213,10 @@ class PDP(keras.layers.Layer):
             mask = self.mask
         else:
             if self.config.structured_pruning:
-                mask = self.get_mask_structured(weight)
+                if self.layer_type == "conv":
+                    mask = self.get_mask_structured_channel(weight)
+                else:
+                    mask = self.get_mask_structured_linear(weight)
             else:
                 mask = self.get_mask(weight)
         return mask * weight
@@ -284,6 +378,7 @@ class DST(keras.layers.Layer):
         threshold_size = get_threshold_size(config, out_size, layer.weight.shape)
         self.threshold = self.add_weight(shape = threshold_size, initializer="zeros", trainable=True)
         self.config = config
+        self.mask = torch.ones(layer.weight.shape, requires_grad=False)
 
     def call(self, weight):
         """
