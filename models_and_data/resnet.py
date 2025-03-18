correct averaging of inputs for Wanda and CL, fix resnet print

models_and_data/resnet.py
@@ -160,8 +160,8 @@ def validate_resnet(model, testloader, device, loss_func, epoch, writer, *args, 
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-            ratio = get_layer_keep_ratio(model)
-            print(f'Accuracy: {100 * correct / total}%, remaining_weights: {ratio * 100}%')
+        ratio = get_layer_keep_ratio(model)
+        print(f'Accuracy: {100 * correct / total:.2f}%, remaining_weights: {ratio * 100:.2f}%')
         if writer is not None:
             writer.add_scalar("validation_output_loss", loss.item(), epoch)
             writer.add_scalar("validation_sparse_loss", losses, epoch)

pquant/pruning_methods/wanda.py
@@ -78,23 +78,30 @@ class Wanda(nn.Module):
             For conv layers, take average over batch dimension and calculate norm over flattened kernel_size dimension.
             If N and M are defined, do N:M pruning.
             """
-            if not self.training or x.shape[0] != self.config["training_parameters"]["batch_size"]:
+            ok_batch = True
+            if self.inputs is not None:
+                batch_size = self.inputs.shape[0]
+                ok_batch = x.shape[0] == batch_size
+            if not self.training or not ok_batch:
                 # Don't collect during validation
                 return
             self.t += 1
             if self.t < self.t_start_collecting:
                 return
-            self.total += x.shape[0]
+            self.total += 1
             self.inputs = x if self.inputs is None else self.inputs + x
 
             if self.t % (self.t_start_collecting + self.config["pruning_parameters"]["t_delta"]) == 0:
                 inputs_avg = self.inputs / self.total
-                if self.layer_type == "linear":
-                    self.handle_linear(inputs_avg)
-                else:
-                    self.handle_conv(inputs_avg)
+                self.prune(inputs_avg)
                 self.done = True
                 self.inputs = None
+
+    def prune(self, x):
+        if self.layer_type == "linear":
+            self.handle_linear(x)
+        else:
+            self.handle_conv(x)
 
     def build(self, weight):
         # Since this is a torch layer, do nothing