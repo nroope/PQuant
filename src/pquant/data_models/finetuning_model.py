f2e1887675ee3b8744a1900651cabc629e1d1bd4
Anastasiia <anastasiia.petrovych@ucu.edu.ua>
2025-10-14 19:14:58 +0300

    Update finetuning_model.py

--- src/pquant/data_models/finetuning_model.py
+++ src/pquant/data_models/finetuning_model.py
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 from typing import Dict, List, Optional, Union, Any
-
 from pydantic import BaseModel, Field
 from typing_extensions import Literal
-=======
 from typing import Dict, List, Optional, Union
-
 from pydantic import BaseModel, Field
-
->>>>>>> d8cc713 (Add fine-tuning feature v1)
 
 class HyperparameterSearch(BaseModel):
     numerical: Dict[str, List[Union[int, float]]] = Field(default_factory=dict)
     categorical: Optional[Dict[str, List[str]]] = Field(default_factory=dict)
 
-<<<<<<< HEAD
 class Sampler(BaseModel):
     type: str = Field(default="TPESampler")
     params: Dict[str, Any] = Field(default_factory=dict)
@@ -23,11 +16,9 @@ class Sampler(BaseModel):
 class BaseFinetuningModel(BaseModel):
     experiment_name: str = Field(default="experiment_1")
     sampler: Sampler = Field(default_factory=Sampler)
-=======
 
 class BaseFinetuningModel(BaseModel):
     experiment_name: str = Field(default="experiment_1")
     sampler: str = Field(default="TPESampler")
->>>>>>> d8cc713 (Add fine-tuning feature v1)
     num_trials: int = Field(default=0)
     hyperparameter_search: HyperparameterSearch = Field(default_factory=HyperparameterSearch)
