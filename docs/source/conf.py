4d370c2828a2fe52c87df13bebe0e915d17d32f3
Anastasiia Petrovych <anastasiyapetrovych25@gmail.com>
2025-12-08 16:11:38 +0100

    Add modifications

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,67 +1,72 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+import pquant
+
+sys.path.insert(0, os.path.abspath('../'))
 
 project = 'PQuantML'
-copyright = '2025, Roope Niemi, Anastasiia Petrovych'
+copyright = '2025, Roope Niemi'
 author = 'Roope Niemi, Anastasiia Petrovych'
-
-# The full version, including alpha/beta/rc tags
-release = '15.12.2025'
-
+release = str(pquant.__version__)
+version = release
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "myst_parser",
+myst_enable_extensions = [
+    "amsmath",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
 ]
 
-source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
 
+autosummary_generate = True
 
-# Add any paths that contain templates here, relative to this directory.
+extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
+
+source_suffix = ['.rst', '.md']
+
 templates_path = ['_templates']
+exclude_patterns = ['_build']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "furo"
 html_logo = "_static/pquant.png"
 html_theme_options = {
-    "sidebar_hide_name": True,
+    'logo_only': True,
+    'display_version': True,
 }
 
+html_context = {
+    'display_github': True,  # Integrate GitHub
+    'github_user': 'nroope',  # Username
+    'github_repo': "PQuant",  # Repo name
+    'github_version': 'master',  # Version
+    'conf_py_path': '/docs/',  # Path in the checkout to the docs root
+}
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_favicon = '_static/pquant.png'
+
+html_css_files = [
+    'custom.css',
+]
